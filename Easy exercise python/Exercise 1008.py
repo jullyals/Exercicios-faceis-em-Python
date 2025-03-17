485a3b31b133e72641c681a57eef7a0430ffb380
Jully <jullyaldasilva@gmail.com>
2025-03-17 14:13:08 -0300

    Update Exercise 1008.py

--- Easy exercise python/Exercise 1008.py
+++ Easy exercise python/Exercise 1008.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+#nº de funcionários, nº de h trabalhadas e valor salário/h e o valor total
+n=int(input()) #ID do funcionário
+h=int(input()) #horas
+salhora=float(input()) #salario/hora
+sal=h*salhora
+print(f"NUMBER = {n}")
+print(f"SALARY = U$ {sal:.2f}")
