4e348549562e7a4f28d2e184b52e2b7ca3203206
Jully <jullyaldasilva@gmail.com>
2025-03-17 14:18:51 -0300

    Update Exercise 1016.py

---
+++ Easy exercise python/Exercise 1016.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#Dados do problema:
+# Dois carros X e Y na mesma direção, com velocidades de 60km/h e 90km/h, respectivamente.
+#Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.
+x=int(input())
+y=2*x           #Y consegue se afastar um quilômetro a cada 2 minutos de X
+print(f"{y} minutos")
