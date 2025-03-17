5147e332b149787b787924a3640dcb1e126efecd
Jully <jullyaldasilva@gmail.com>
2025-03-17 14:18:11 -0300

    Update Exercise 1014.py

---
+++ Easy exercise python/Exercise 1014.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#consumo médio do automóvel
+x=int(input())      #distância total percorrida (km)
+y=float(input())    #total de combustível gasto (L)
+total=x/y
+print(f"{total:.3f} km/l")
